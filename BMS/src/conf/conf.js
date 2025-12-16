1c6718fc7da50cb612723c1c2a3d6b579ae277f7
Rupjyoti Barman <rupjyoti.barman@hp.com>
2025-12-16 22:41:40 +0530

    created env variables for appwrite

---
+++ BMS/src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteProjectName: String(import.meta.env.VITE_APPWRITE_PROJECT_NAME),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf;
