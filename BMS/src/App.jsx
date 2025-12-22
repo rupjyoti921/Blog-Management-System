fe2a6130a3e02e220072473f9c4c5dfece3fc3e3
Rupjyoti Barman <rupjyoti.barman@hp.com>
2025-12-22 23:29:29 +0530

    created the store

--- BMS/src/App.jsx
+++ BMS/src/App.jsx
@@ -1,11 +1,48 @@
+import './App.css'
+import { useState, useEffect, use } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth/auth';
+import {login,logout} from './store/authSlice';
+import {Header, Footer} from './components/index';
+
 
 function App() {
 
-  return (
-    <>
-      <h1> Hello Guys</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((user) => {
+        if(user){
+            dispatch(login(user));
+        }
+        else{
+          dispatch(logout());
+        }
+    })
+    .catch(()=>{
+      dispatch(logout());
+    })
+    .finally(()=>{
+      setLoading(false);
+    })
+  }, []);
+
+
+return loading ? (
+  <div>
+    <h2>Loading...</h2>
+  </div>
+):(
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header />
+      <Footer />
+    </div>
+  </div>
+);
 }
+
 
 export default App
